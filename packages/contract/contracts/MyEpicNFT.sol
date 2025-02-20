2c1359fa0eb95a21fe7364a367e6e77ecc7ffc21
TZKJ-univ <soeda-kojiro@ed.tmu.ac.jp>
2025-02-20 14:57:11 +0900

    feat: frontend

--- packages/contract/contracts/MyEpicNFT.sol
+++ packages/contract/contracts/MyEpicNFT.sol
@@ -15,11 +15,13 @@ contract MyEpicNFT is ERC721URIStorage {
 
     Counters.Counter private _tokenIds;
 
-    string baseSvg = "<svg xmlns='https://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base {fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base {fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     string[] firstWords = ["Boop", "Bop", "Bee", "Bar", "Baz", "Bro"];
     string[] secondWords = ["Cry", "Cli", "CSV", "Cup", "Cip", "Cre"];
     string[] thirdWords = ["Dop", "Dip", "Dap", "Dpp", "Dup", "Dep"];
+
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
  
     constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract.");
@@ -29,8 +31,8 @@ contract MyEpicNFT is ERC721URIStorage {
         return uint256(keccak256(abi.encodePacked(input)));
     }
 
-    function pickRandomFirstWord(uint256 tokenID) public view returns (string memory) {
-        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenID))));
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
         console.log("rand seed: ", rand);
 
         rand = rand % firstWords.length;
@@ -39,16 +41,16 @@ contract MyEpicNFT is ERC721URIStorage {
         return firstWords[rand];
     }
 
-    function pickRandomSecondWord(uint256 tokenID) public view returns (string memory) {
-        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenID))));
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
 
         rand = rand % secondWords.length;
 
         return secondWords[rand];
     }
 
-    function pickRandomThirdWord(uint256 tokenID) public view returns (string memory) {
-        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenID))));
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
 
         rand = rand % thirdWords.length;
 
@@ -102,5 +104,6 @@ contract MyEpicNFT is ERC721URIStorage {
         );
 
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
